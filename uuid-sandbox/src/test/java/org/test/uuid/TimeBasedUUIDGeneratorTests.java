40db27454348f0a024b98594636ead59c659373b
Oleg Zhurakousky <ozhurakousky@vmware.com>
2011-05-13 13:34:21 -0400

    initial commit

---
+++ uuid-sandbox/src/test/java/org/test/uuid/TimeBasedUUIDGeneratorTests.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2002-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.test.uuid;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.util.StopWatch;
+
+/**
+ * @author Oleg Zhurakousky
+ *
+ */
+public class TimeBasedUUIDGeneratorTests {
+	private long  macAddressAsLong = 0;
+
+	@Test
+	public void testGreaterThen(){
+		UUID id = TimeBasedUUIDGenerator.generateId();
+		for (int i = 0; i < 10; i++) {
+			UUID newId = TimeBasedUUIDGenerator.generateId();
+			// tests, that newly created UUID is always greater then the previous one.
+			Assert.assertTrue(newId.compareTo(id) >= 0);
+			id = newId;
+		}
+	}
+	@Test
+	public void testUniqueness(){
+		long timestamp = System.currentTimeMillis();
+		UUID id = TimeBasedUUIDGenerator.generateIdFromTimestamp(timestamp);
+		for (int i = 0; i < 32000; i++) { //32767 max
+			UUID newId = TimeBasedUUIDGenerator.generateIdFromTimestamp(timestamp);
+			// tests, that newly created UUID is always greater then the previous one.
+			//System.out.println(Long.toBinaryString(id.clockSequence()));
+			Assert.assertTrue(newId.compareTo(id) == 1);
+			Assert.assertEquals(newId.timestamp(), id.timestamp());
+			id = newId;
+		}
+	}
+	@Test
+	public void testStructure(){
+		long timestamp = System.currentTimeMillis();
+		UUID id = TimeBasedUUIDGenerator.generateIdFromTimestamp(timestamp);
+		for (int i = 0; i < 30000; i++) {
+			UUID newId = TimeBasedUUIDGenerator.generateIdFromTimestamp(timestamp);
+			id = newId;
+		}
+		Assert.assertEquals(timestamp, id.timestamp());
+		Assert.assertEquals(macAddressAsLong, id.node());
+	}
+	@Test
+	public void performanceTestSynch(){
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
+		for (int i = 0; i < 100000; i++) {
+			TimeBasedUUIDGenerator.generateId();
+		}
+		stopWatch.stop();
+		System.out.println("Generated 100000 UUID (sync) via TimeBasedUUIDGenerator.generateId(): in " + stopWatch.getTotalTimeSeconds() + " seconds");
+		
+		stopWatch = new StopWatch();
+		stopWatch.start();
+		for (int i = 0; i < 100000; i++) {
+			UUID.randomUUID();
+		}
+		stopWatch.stop();
+		System.out.println("Generated 100000 UUID  (sync) via UUID.randomUUID(): in " + stopWatch.getTotalTimeSeconds() + " seconds");
+	}
+	@Test
+	public void performanceTestAsynch() throws Exception {
+		ExecutorService executor = Executors.newFixedThreadPool(100);
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
+		for (int i = 0; i < 100000; i++) {
+			executor.execute(new Runnable() {
+				public void run() {
+					TimeBasedUUIDGenerator.generateId();
+				}
+			});
+		}
+		executor.shutdown();
+		executor.awaitTermination(10, TimeUnit.SECONDS);
+		stopWatch.stop();
+		System.out.println("Generated 100000 UUID (async) via TimeBasedUUIDGenerator.generateId(): in " + stopWatch.getTotalTimeSeconds() + " seconds");
+		
+		executor = Executors.newFixedThreadPool(100);
+		stopWatch = new StopWatch();
+		stopWatch.start();
+		for (int i = 0; i < 100000; i++) {
+			executor.execute(new Runnable() {
+				public void run() {
+					UUID.randomUUID();
+				}
+			});
+		}
+		executor.shutdown();
+		executor.awaitTermination(10, TimeUnit.SECONDS);
+		stopWatch.stop();
+		System.out.println("Generated 100000 UUID (async) via UUID.randomUUID(): in " + stopWatch.getTotalTimeSeconds() + " seconds");
+	}
+	@Before
+	public void prepare(){
+		
+		try {
+			InetAddress address = InetAddress.getLocalHost();
+			NetworkInterface ni = NetworkInterface.getByInetAddress(address);
+			if (ni != null) {
+				byte[] mac = ni.getHardwareAddress();
+				//Converts array of unsigned bytes to an long
+				if (mac != null) {
+					for (int i = 0; i < mac.length; i++) {					
+						macAddressAsLong <<= 8;
+						macAddressAsLong ^= (long)mac[i] & 0xFF;
+					}
+				}
+			} 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ uuid-sandbox/target/test-classes/org/test/uuid/.svn/text-base/BitwiseTest.java.svn-base
@@ -0,0 +1,112 @@
+package org.test.uuid;
+
+import java.lang.management.ManagementFactory;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.security.SecureRandom;
+import java.util.Date;
+import java.util.UUID;
+
+public class BitwiseTest {
+	public static final Object lock = new Object();
+	private static long clockSequence;
+	private static long lastTime;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		long currentTime = System.currentTimeMillis();
+		long nanos = System.nanoTime() | new SecureRandom(ManagementFactory.getRuntimeMXBean().getName().getBytes()).nextLong();
+		nanos = 0;
+		// msb
+		long time_low = (currentTime & 0xFFFFFFFFL);
+		time_low <<= 32;
+	
+	    long time_mid = (currentTime & 0xFFFFFFFF00000000L) >> 16;
+		long time_hi_and_version = ((currentTime >> 48) & 0x0FFF);
+	    time_hi_and_version |= (1 << 12);
+	    long msb = time_low | time_mid | time_hi_and_version;
+		
+
+		long variant = 2;
+		variant <<= 62;
+		//System.out.println(Long.toBinaryString(variant));
+		
+		long clockSequence = nanos & 0x000000000000FFFFL;
+		clockSequence <<= 48;
+		//System.out.println(Long.toBinaryString(clockSequence));
+		
+		//System.out.println(Long.toBinaryString(variant | clockSequence));
+		
+		long mac = getNode();
+		//System.out.println(Long.toBinaryString(mac));
+		long lsb = variant | clockSequence | mac;
+		//System.out.println(Long.toBinaryString(lsb));
+		
+		UUID id = new UUID(msb, lsb);
+		for (int i = 1; i < 100000; i++) {
+			clockSequence += i;
+			clockSequence &= 0x000000000000FFFFL;
+			clockSequence <<= 48;
+			//System.out.println(Long.toBinaryString(clockSequence));
+			lsb = variant | clockSequence | mac;
+			//System.out.println(Long.toBinaryString(lsb));
+			//sSystem.out.println(id.clockSequence());
+			UUID idA = new UUID(msb, lsb);
+			if (idA.compareTo(id) < 1){
+				throw new RuntimeException(idA.clockSequence()+ " + " + i);
+			}
+			//System.out.println(idA.clockSequence());
+			id = idA;
+		}
+
+		System.out.println(id);
+		System.out.println(id.variant());
+		System.out.println(id.version());
+		System.out.println(new Date(id.timestamp()));
+		System.out.println(Long.toBinaryString(id.node()));
+	}
+	
+	public static long getSecond(int pid, long currentTime){
+		synchronized (lock) {
+			if (currentTime > lastTime){
+				lastTime = currentTime;
+				clockSequence = 0;
+				System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+			} else {
+				++clockSequence;
+			}
+				
+			return (pid|clockSequence) << 48;
+		}
+	}
+	
+	public static long getNode(){
+		long macAddressAsLong = 0;
+		try {
+			int processClIdentity = new SecureRandom(ManagementFactory.getRuntimeMXBean().getName().getBytes()).nextInt();
+//			macAddressAsLong <<= 8;
+//			macAddressAsLong ^= (long)processClIdentity & 0xFF;
+//			System.out.println("id: " + Long.toBinaryString(macAddressAsLong));
+			
+			InetAddress address = InetAddress.getLocalHost();
+			NetworkInterface ni = NetworkInterface.getByInetAddress(address);
+			if (ni != null) {
+				byte[] mac = ni.getHardwareAddress();
+				
+				//Converts array of unsigned bytes to an long
+				if (mac != null) {
+					for (int i = 0; i < mac.length; i++) {					
+						macAddressAsLong <<= 8;
+						macAddressAsLong ^= (long)mac[i] & 0xFF;
+						//System.out.println(Long.toBinaryString(macAddressAsLong));
+					}
+				}
+			} 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return macAddressAsLong ;
+	}
+
+}
